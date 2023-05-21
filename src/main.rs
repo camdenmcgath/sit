d47ea6b075afefb77ccaccc90bffa051e5f4b78f
camocodes <clm2930@comcast.net>
2023-05-20 17:23:13 -0700

    batman

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,46 @@
 use clap::Parser;
 use std::io::{self, Write};
 use std::process::Command;
+use std::vec;
 
+//TODO: GOAL: get basic functionality (git add . , commit, push)
+//TODO: NEXT: organize (module for each combo), add support for other os/shells, pretty print
 #[derive(Parser, Debug)]
 struct Args {
     ///Spit command name mapping to a combo of git commands
     cmd: String,
 
     /// message to be passed to -m in git command
-    message: String,
+    msg: Option<String>,
 }
 
+fn get_sequence(cmd: String, msg: Option<String>) -> Result<Vec<&'static str>, &'static str> {
+    match cmd.as_str() {
+        "update" => {
+            if let Some(message) = msg {
+                Ok(vec![
+                    "git add .",
+                    format!("git commit -m \"{}\"", message).as_str(),
+                    "git push origin master",
+                ])
+            } else {
+                Ok(vec![
+                    "git add .",
+                    "git commit -m \"\"",
+                    "git push origin master",
+                ])
+            }
+        }
+        _ => Err("Unknown command."),
+    }
+}
 fn main() {
     let args = Args::parse();
-
+    let sequence = get_sequence(args.cmd, args.msg).unwrap();
+    //TODO: iterate through sequence and run each command (below code) each time
     let output = Command::new("powershell")
         .arg("-Command")
-        .arg(args.cmd)
+        .arg(sequence)
         .output()
         .expect("Failed to execute command");
 
