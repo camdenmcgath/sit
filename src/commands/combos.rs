41116dfde95cd2ef0eaf67710c1814ca6e042dce
camocodes <clm2930@comcast.net>
2023-07-19 09:05:53 -0600

    update

--- src/commands/combos.rs
+++ src/commands/combos.rs
@@ -3,10 +3,7 @@ use crate::Args;
 pub fn commit(args: Args) -> Vec<String> {
     vec![
         "git add .".to_string(),
-        format!(
-            "git commit -m {}",
-            args.msg.unwrap_or("defualt".to_string())
-        ),
+        format!("git commit -m {}", args.msg.unwrap_or("update".to_string())),
     ]
 }
 
