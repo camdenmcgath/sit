621b9eae1aabc4bae3c9853ead9998c94f9a24bf
camocodes <clm2930@comcast.net>
2023-07-19 14:24:16 -0600

    update

--- src/tests/tests.rs
+++ src/tests/tests.rs
@@ -1,26 +1,73 @@
+use std::fmt::format;
+use std::vec;
+
 use crate::commands::combos::*;
 use crate::Args;
+
+fn combo_test(args: Args, expected: Vec<String>) {
+    let matched = get_combo(args.clone());
+    let combo = get_combo(args.clone());
+    assert_eq!(
+        matched, combo,
+        "get_combo function failed to match {}",
+        args.cmd
+    );
+    assert_eq!(
+        combo, expected,
+        "Did not get expected commit combo: got {:?}, expected {:?}",
+        combo, expected
+    );
+}
 
 #[test]
 fn check_current_branch() {
     assert_eq!(current_branch(), "master")
 }
+
 #[test]
-fn check_combo_match() {
-    let commit_args = Args {
-        cmd: String::from("commit"),
-        msg: Some(String::from("testing")),
-        url: None,
-    };
-    assert_eq!(get_combo(commit_args.clone()), commit(commit_args));
-
-    let push_args = Args {
-        cmd: String::from("push"),
-        msg: None,
-        url: None,
-    };
-    assert_eq!(get_combo(push_args), push());
-
-    //TODO: finish tests
-    //TODO: add test by running the CLI in the main dir
+fn check_commit() {
+    combo_test(
+        Args {
+            cmd: String::from("commit"),
+            msg: Some(String::from("testing")),
+            url: None,
+        },
+        vec![
+            String::from("git add ."),
+            String::from("git commit --message=\"testing\""),
+        ],
+    );
 }
+
+#[test]
+fn check_push() {
+    combo_test(
+        Args {
+            cmd: String::from("push"),
+            msg: None,
+            url: None,
+        },
+        vec![format!(
+            "git push --set-upstream origin {}",
+            current_branch()
+        )],
+    );
+}
+
+#[test]
+fn check_update() {
+    combo_test(
+        Args {
+            cmd: String::from("update"),
+            msg: Some(String::from("testing")),
+            url: None,
+        },
+        vec![
+            String::from("git add ."),
+            String::from("git commit --message=\"testing\""),
+            format!("git push --set-upstream origin {}", current_branch()),
+        ],
+    );
+}
+
+//TODO: add test by running the CLI in the main dir
